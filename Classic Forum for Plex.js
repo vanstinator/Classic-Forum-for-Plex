dc14545f34251c1b18fb140fe3973d21435528d2
Justin Vanderhooft <justinvdhooft@gmail.com>
2015-08-05 22:24:00 -0400

    updating DOM and adding changes to support image loading

--- Classic Forum for Plex.js
+++ Classic Forum for Plex.js
@@ -1,73 +1,124 @@
 // ==UserScript==
 // @name         Classic Forum for Plex
 // @namespace    forums.plex.tv
-// @include      http://forums.plex.tv/*
-// @include      https://forums.plex.tv/*
-// @version      0.1
+// @version      0.2
 // @description  The JavaScript portion of the skin. Find the CSS portion at https://userstyles.org/styles/117161/classic-forum-for-plex
 // @author       Justin Vanderhooft
-// @match        http://userscripts.org/
+// @match        forums.plex.tv/*
 // @grant        none
 // ==/UserScript==
 
-var commentElements = document.getElementsByClassName('Item ItemComment Online');
+//Variables used by everything
+var localstorage = window['localStorage'];
+buildCache();
+setCustomClasses();
+layoutDiscussionDOM();
+/*layoutCategoryDOM();*/
+
+/*var commentElements = document.getElementsByClassName('Item ItemComment Online');
 for(var i = 0; i < commentElements.length; i++)
 {
     var authorElements = commentElements[i].getElementsByClassName('Author');
     for(var j = 0; j < authorElements.length; j++) {
         $("<span class=\"MItem CustSkin-OnlineStatus\">Online</span>").appendTo(authorElements[j]);
     }
+}*/
+
+function setCustomClasses() {
+    var discussionElements = document.getElementsByClassName('ItemDiscussion');
+    for(var i = 0; i < discussionElements.length; i++) {
+        var photo = discussionElements[i].getElementsByClassName('IndexPhoto PhotoWrap')[0];
+
+        $(photo).addClass('CustSkin-Remove');
+    }
+
 }
 
-var discussionElements = document.getElementsByClassName('ItemDiscussion');
-for(var i = 0; i < discussionElements.length; i++) {
-    var title = discussionElements[i].getElementsByClassName('Title')[0];
-    var photo = discussionElements[i].getElementsByClassName('IndexPhoto PhotoWrap')[0]
-    var options = discussionElements[i].getElementsByClassName('Options')[0];
-    var answeredTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Accepted')[0];
-    var answerTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Answered')[0];
-    var questionTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Question')[0];
-    var announcementTag = discussionElements[i].getElementsByClassName('Tag Tag-Announcement')[0]; 
-    var closedTag = discussionElements[i].getElementsByClassName('Tag Tag-Closed')[0];
-    var author = discussionElements[i].getElementsByClassName('MItem DiscussionAuthor')[0];
-    var viewCount = discussionElements[i].getElementsByClassName('MItem ViewCount')[0];
-    var commentCount = discussionElements[i].getElementsByClassName('MItem CommentCount')[0];
-    var discussionScore = discussionElements[i].getElementsByClassName('MItem DiscussionScore')[0];
-    var lastComment = discussionElements[i].getElementsByClassName('MItem LastCommentBy')[0];
-    var lastCommentDate = discussionElements[i].getElementsByClassName('MItem LastCommentDate')[0];
+function layoutDiscussionDOM() {
+    var discussionElements = document.getElementsByClassName('ItemDiscussion');
+    for(var i = 0; i < discussionElements.length; i++) {
+        var title = discussionElements[i].getElementsByClassName('Title')[0];
+        var photo = discussionElements[i].getElementsByClassName('IndexPhoto PhotoWrap')[0];
+        var options = discussionElements[i].getElementsByClassName('Options')[0];
+        var answeredTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Accepted')[0];
+        var answerTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Answered')[0];
+        var questionTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Question')[0];
+        var announcementTag = discussionElements[i].getElementsByClassName('Tag Tag-Announcement')[0]; 
+        var closedTag = discussionElements[i].getElementsByClassName('Tag Tag-Closed')[0];
+        var author = discussionElements[i].getElementsByClassName('MItem DiscussionAuthor')[0];
+        var viewCount = discussionElements[i].getElementsByClassName('MItem ViewCount')[0];
+        var commentCount = discussionElements[i].getElementsByClassName('MItem CommentCount')[0];
+        var discussionScore = discussionElements[i].getElementsByClassName('MItem DiscussionScore')[0];
+        var lastComment = discussionElements[i].getElementsByClassName('MItem LastCommentBy')[0];
+        var lastCommentDate = discussionElements[i].getElementsByClassName('MItem LastCommentDate')[0];
+        var hasNewComment = discussionElements[i].getElementsByClassName('HasNew NewCommentCount')[0];
+        var itemContent = discussionElements[i].getElementsByClassName('ItemContent')[0];
 
-    $("<div class=\".col-md-8 Meta CustSkin-LeftDiscussionColumn\"></div>").appendTo(discussionElements[i]);
-    $("<div class=\".col-md-4 Meta CustSkin-RightDiscussionColumn\"></div>").appendTo(discussionElements[i]);
+        $("<div class=\".col-md-8 Meta CustSkin-LeftDiscussionColumn\"></div>").appendTo(discussionElements[i]);
+        $("<div class=\".col-md-4 Meta CustSkin-RightDiscussionColumn\"></div>").appendTo(discussionElements[i]);
 
-    var leftCol = discussionElements[i].getElementsByClassName('CustSkin-LeftDiscussionColumn')[0];
-    var rightCol = discussionElements[i].getElementsByClassName('CustSkin-RightDiscussionColumn')[0];
+        var leftCol = discussionElements[i].getElementsByClassName('CustSkin-LeftDiscussionColumn')[0];
+        var rightCol = discussionElements[i].getElementsByClassName('CustSkin-RightDiscussionColumn')[0];
 
-    $(photo).appendTo(leftCol);
-    $(options).appendTo(rightCol);
 
-    $("<div class=\"ItemContent Discussion\"><\div>").appendTo(leftCol);
-    $("<div class=\"ItemContent Discussion\"><\div>").appendTo(rightCol);
 
-    var leftDiscussionDiv = leftCol.getElementsByClassName('ItemContent Discussion')[0];
-    var rightDiscussionDiv = rightCol.getElementsByClassName('ItemContent Discussion')[0];
+        $(photo).appendTo(leftCol);
+        $(options).appendTo(rightCol);
 
-    $(title).appendTo(leftDiscussionDiv);
+        try {
+            var lastCommentedUsername = lastComment.getElementsByTagName('a')[0].text;
+            $("<a title="+lastCommentedUsername+ " href=\"/profile/"+lastCommentedUsername+"\" class=\"Index Photo PhotoWrap CustSkin-PhotoWrap Online Rank-Admin\"><img src=\""+localstorage[lastCommentedUsername]+"\" alt=\""+lastCommentedUsername+"\" class=\"ProfilePhoto ProfilePhotoMedium\"></a>").appendTo(rightCol);
+        } catch (e) {
+            console.log("no image for this user");   
+        }
 
-    $("<div class=\"Meta Meta-Discussion\"><\div>").appendTo(leftDiscussionDiv);
-    $("<div class=\"Meta Meta-Discussion\"><\div>").appendTo(rightDiscussionDiv);
+        $("<div class=\"ItemContent Discussion CustSkin-ItemContent\"><\div>").appendTo(leftCol);
+        $("<div class=\"ItemContent Discussion CustSkin-ItemContent\"><\div>").appendTo(rightCol);
 
-    var leftMetaDiv = leftDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
-    var rightMetaDiv = rightDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
+        var leftDiscussionDiv = leftCol.getElementsByClassName('ItemContent Discussion')[0];
+        var rightDiscussionDiv = rightCol.getElementsByClassName('ItemContent Discussion')[0];
 
-    $(answeredTag).appendTo(leftMetaDiv);
-    $(answerTag).appendTo(leftMetaDiv);
-    $(questionTag).appendTo(leftMetaDiv);
-    $(announcementTag).appendTo(leftMetaDiv);
-    $(closedTag).appendTo(leftMetaDiv);
-    $(author).appendTo(leftMetaDiv);
-    $(viewCount).appendTo(leftMetaDiv);
-    $(commentCount).appendTo(leftMetaDiv);
-    $(discussionScore).appendTo(leftMetaDiv);
-    $(lastComment).appendTo(rightMetaDiv);
-    $(lastCommentDate).appendTo(rightMetaDiv);
+        $(title).appendTo(leftDiscussionDiv);
+
+        $("<div class=\"Meta Meta-Discussion\"><\div>").appendTo(leftDiscussionDiv);
+        $("<div class=\"Meta Meta-Discussion CustSkin-LatestCommenter\"><\div>").appendTo(rightDiscussionDiv);
+
+        var leftMetaDiv = leftDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
+        var rightMetaDiv = rightDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
+
+        $(answeredTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+        $(answerTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+        $(questionTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+        $(announcementTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+        $(closedTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+        $(hasNewComment).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+        $(author).appendTo(leftMetaDiv);
+        $(viewCount).appendTo(leftMetaDiv);
+        $(commentCount).appendTo(leftMetaDiv);
+        $(discussionScore).appendTo(leftMetaDiv);
+        $(lastComment).appendTo(rightMetaDiv);
+        $(lastCommentDate).appendTo(rightMetaDiv);
+        $(itemContent).remove();
+
+    }
+}
+
+function layoutCategoryDOM() {
+    var categoryElements = document.getElementsByClassName('Item');
+    for(var i = 0; i < categoryElements.length; i++) {
+        if(!$(categoryElements[i]).hasClass('CategoryHeading')) {
+            /*console.log('t');*/
+        }
+    }
+}
+
+function buildCache() {
+    //Setup local storage so we can store a map of usernames and avatars
+    var profilePhotos = document.getElementsByClassName('ProfilePhoto ProfilePhotoMedium');
+    for(var i = 0; i < profilePhotos.length; i++) {
+        var imgUrl = profilePhotos[i].src;
+        var username = profilePhotos[i].alt;
+        localstorage[username] = imgUrl;
+    }
+    console.log("There are " + localstorage.length + " urls cached");
 }
