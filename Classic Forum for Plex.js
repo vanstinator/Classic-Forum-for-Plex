e9d58cf5fccca6dff04611e062ee808f64c1d869
Justin Vanderhooft <justinvdhooft@gmail.com>
2015-08-06 23:40:12 -0400

    adding code to modify the homepage

--- Classic Forum for Plex.js
+++ Classic Forum for Plex.js
@@ -13,7 +13,9 @@ var localstorage = window['localStorage'];
 buildCache();
 setCustomClasses();
 layoutDiscussionDOM();
-/*layoutCategoryDOM();*/
+layoutCategoryDOM();
+
+
 
 /*var commentElements = document.getElementsByClassName('Item ItemComment Online');
 for(var i = 0; i < commentElements.length; i++)
@@ -35,79 +37,135 @@ function setCustomClasses() {
 }
 
 function layoutDiscussionDOM() {
-    var discussionElements = document.getElementsByClassName('ItemDiscussion');
-    for(var i = 0; i < discussionElements.length; i++) {
-        var title = discussionElements[i].getElementsByClassName('Title')[0];
-        var photo = discussionElements[i].getElementsByClassName('IndexPhoto PhotoWrap')[0];
-        var options = discussionElements[i].getElementsByClassName('Options')[0];
-        var answeredTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Accepted')[0];
-        var answerTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Answered')[0];
-        var questionTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Question')[0];
-        var announcementTag = discussionElements[i].getElementsByClassName('Tag Tag-Announcement')[0]; 
-        var closedTag = discussionElements[i].getElementsByClassName('Tag Tag-Closed')[0];
-        var author = discussionElements[i].getElementsByClassName('MItem DiscussionAuthor')[0];
-        var viewCount = discussionElements[i].getElementsByClassName('MItem ViewCount')[0];
-        var commentCount = discussionElements[i].getElementsByClassName('MItem CommentCount')[0];
-        var discussionScore = discussionElements[i].getElementsByClassName('MItem DiscussionScore')[0];
-        var lastComment = discussionElements[i].getElementsByClassName('MItem LastCommentBy')[0];
-        var lastCommentDate = discussionElements[i].getElementsByClassName('MItem LastCommentDate')[0];
-        var hasNewComment = discussionElements[i].getElementsByClassName('HasNew NewCommentCount')[0];
-        var itemContent = discussionElements[i].getElementsByClassName('ItemContent')[0];
+    if(document.location.pathname.startsWith('/categories',0)) {
+        var discussionElements = document.getElementsByClassName('ItemDiscussion');
+        for(var i = 0; i < discussionElements.length; i++) {
+            var title = discussionElements[i].getElementsByClassName('Title')[0];
+            var photo = discussionElements[i].getElementsByClassName('IndexPhoto PhotoWrap')[0];
+            var options = discussionElements[i].getElementsByClassName('Options')[0];
+            var answeredTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Accepted')[0];
+            var answerTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Answered')[0];
+            var questionTag = discussionElements[i].getElementsByClassName('Tag QnA-Tag-Question')[0];
+            var announcementTag = discussionElements[i].getElementsByClassName('Tag Tag-Announcement')[0]; 
+            var closedTag = discussionElements[i].getElementsByClassName('Tag Tag-Closed')[0];
+            var author = discussionElements[i].getElementsByClassName('MItem DiscussionAuthor')[0];
+            var viewCount = discussionElements[i].getElementsByClassName('MItem ViewCount')[0];
+            var commentCount = discussionElements[i].getElementsByClassName('MItem CommentCount')[0];
+            var discussionScore = discussionElements[i].getElementsByClassName('MItem DiscussionScore')[0];
+            var lastComment = discussionElements[i].getElementsByClassName('MItem LastCommentBy')[0];
+            var lastCommentDate = discussionElements[i].getElementsByClassName('MItem LastCommentDate')[0];
+            var hasNewComment = discussionElements[i].getElementsByClassName('HasNew NewCommentCount')[0];
+            var itemContent = discussionElements[i].getElementsByClassName('ItemContent')[0];
 
-        $("<div class=\".col-md-8 Meta CustSkin-LeftDiscussionColumn\"></div>").appendTo(discussionElements[i]);
-        $("<div class=\".col-md-4 Meta CustSkin-RightDiscussionColumn\"></div>").appendTo(discussionElements[i]);
+            $("<div class=\".col-md-8 Meta CustSkin-LeftDiscussionColumn\"></div>").appendTo(discussionElements[i]);
+            $("<div class=\".col-md-4 Meta CustSkin-RightDiscussionColumn\"></div>").appendTo(discussionElements[i]);
 
-        var leftCol = discussionElements[i].getElementsByClassName('CustSkin-LeftDiscussionColumn')[0];
-        var rightCol = discussionElements[i].getElementsByClassName('CustSkin-RightDiscussionColumn')[0];
+            var leftCol = discussionElements[i].getElementsByClassName('CustSkin-LeftDiscussionColumn')[0];
+            var rightCol = discussionElements[i].getElementsByClassName('CustSkin-RightDiscussionColumn')[0];
 
 
 
-        $(photo).appendTo(leftCol);
-        $(options).appendTo(rightCol);
+            $(photo).appendTo(leftCol);
+            $(options).appendTo(rightCol);
 
-        try {
-            var lastCommentedUsername = lastComment.getElementsByTagName('a')[0].text;
-            $("<a title="+lastCommentedUsername+ " href=\"/profile/"+lastCommentedUsername+"\" class=\"Index Photo PhotoWrap CustSkin-PhotoWrap Online Rank-Admin\"><img src=\""+localstorage[lastCommentedUsername]+"\" alt=\""+lastCommentedUsername+"\" class=\"ProfilePhoto ProfilePhotoMedium\"></a>").appendTo(rightCol);
-        } catch (e) {
-            console.log("no image for this user");   
+            try {
+                var lastCommentedUsername = lastComment.getElementsByTagName('a')[0].text;
+                $("<a title="+lastCommentedUsername+ " href=\"/profile/"+lastCommentedUsername+"\" class=\"Index Photo PhotoWrap CustSkin-PhotoWrap Online\"><img src=\""+localstorage[lastCommentedUsername]+"\" alt=\""+lastCommentedUsername+"\" class=\"ProfilePhoto ProfilePhotoMedium\"></a>").appendTo(rightCol);
+            } catch (e) {
+                console.log("no image for this user");   
+            }
+
+            $("<div class=\"ItemContent Discussion CustSkin-ItemContent\"><\div>").appendTo(leftCol);
+            $("<div class=\"ItemContent Discussion CustSkin-ItemContent\"><\div>").appendTo(rightCol);
+
+            var leftDiscussionDiv = leftCol.getElementsByClassName('ItemContent Discussion')[0];
+            var rightDiscussionDiv = rightCol.getElementsByClassName('ItemContent Discussion')[0];
+
+            $(title).appendTo(leftDiscussionDiv);
+
+            $("<div class=\"Meta Meta-Discussion\"><\div>").appendTo(leftDiscussionDiv);
+            $("<div class=\"Meta Meta-Discussion CustSkin-LatestCommenter\"><\div>").appendTo(rightDiscussionDiv);
+
+            var leftMetaDiv = leftDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
+            var rightMetaDiv = rightDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
+
+            $(answeredTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+            $(answerTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+            $(questionTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+            $(announcementTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+            $(closedTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+            $(hasNewComment).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
+            $(author).appendTo(leftMetaDiv);
+            $(viewCount).appendTo(leftMetaDiv);
+            $(commentCount).appendTo(leftMetaDiv);
+            $(discussionScore).appendTo(leftMetaDiv);
+            $(lastComment).appendTo(rightMetaDiv);
+            $(lastCommentDate).appendTo(rightMetaDiv);
+            $(itemContent).remove();
+
         }
-
-        $("<div class=\"ItemContent Discussion CustSkin-ItemContent\"><\div>").appendTo(leftCol);
-        $("<div class=\"ItemContent Discussion CustSkin-ItemContent\"><\div>").appendTo(rightCol);
-
-        var leftDiscussionDiv = leftCol.getElementsByClassName('ItemContent Discussion')[0];
-        var rightDiscussionDiv = rightCol.getElementsByClassName('ItemContent Discussion')[0];
-
-        $(title).appendTo(leftDiscussionDiv);
-
-        $("<div class=\"Meta Meta-Discussion\"><\div>").appendTo(leftDiscussionDiv);
-        $("<div class=\"Meta Meta-Discussion CustSkin-LatestCommenter\"><\div>").appendTo(rightDiscussionDiv);
-
-        var leftMetaDiv = leftDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
-        var rightMetaDiv = rightDiscussionDiv.getElementsByClassName('Meta Meta-Discussion')[0];
-
-        $(answeredTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
-        $(answerTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
-        $(questionTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
-        $(announcementTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
-        $(closedTag).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
-        $(hasNewComment).addClass('CustSkin-Tag').appendTo(leftMetaDiv);
-        $(author).appendTo(leftMetaDiv);
-        $(viewCount).appendTo(leftMetaDiv);
-        $(commentCount).appendTo(leftMetaDiv);
-        $(discussionScore).appendTo(leftMetaDiv);
-        $(lastComment).appendTo(rightMetaDiv);
-        $(lastCommentDate).appendTo(rightMetaDiv);
-        $(itemContent).remove();
-
     }
 }
 
 function layoutCategoryDOM() {
-    var categoryElements = document.getElementsByClassName('Item');
-    for(var i = 0; i < categoryElements.length; i++) {
-        if(!$(categoryElements[i]).hasClass('CategoryHeading')) {
-            /*console.log('t');*/
+    if(document.location.pathname == '/') {
+        var categoryElements = document.getElementsByClassName('Item');
+        for(var i = 0; i < categoryElements.length; i++) {
+            if(!$(categoryElements[i]).hasClass('CategoryHeading')) {
+                var category =  categoryElements[i].getElementsByClassName('CustSkin-LeftDiscussionColumn')[0];
+
+                var title = categoryElements[i].getElementsByClassName('TitleWrap')[0];       
+                var options = categoryElements[i].getElementsByClassName('Options')[0];
+                var categoryDescription = categoryElements[i].getElementsByClassName('CategoryDescription')[0];
+                var lastDiscussionTitle = categoryElements[i].getElementsByClassName('MItem LastDiscussionTitle')[0];
+                var lastCommentDate = categoryElements[i].getElementsByClassName('MItem LastCommentDate')[0];
+
+                var meta = categoryElements[i].getElementsByClassName('Meta')[0];
+                var itemContent = categoryElements[i].getElementsByClassName('ItemContent')[0];
+
+
+
+                $("<div class=\".col-md-8 Meta CustSkin-LeftDiscussionColumn\"></div>").appendTo(categoryElements[i]);
+                $("<div class=\".col-md-4 Meta CustSkin-RightDiscussionColumn\"></div>").appendTo(categoryElements[i]);
+
+                var leftCol = categoryElements[i].getElementsByClassName('CustSkin-LeftDiscussionColumn')[0];
+                var rightCol = categoryElements[i].getElementsByClassName('CustSkin-RightDiscussionColumn')[0];
+
+                $(options).appendTo(rightCol);
+
+                try {   
+                    var lastCommentedUsername = lastDiscussionTitle.getElementsByTagName('a')[1].text;
+                    $("<a title="+lastCommentedUsername+ " href=\"/profile/"+lastCommentedUsername+"\" class=\"Index Photo PhotoWrap CustSkin-PhotoWrap Online\"><img src=\""+localstorage[lastCommentedUsername]+"\" alt=\""+lastCommentedUsername+"\" class=\"ProfilePhoto CustSkin-ProfilePhoto ProfilePhotoMedium\"></a>").appendTo(rightCol);
+                } catch (e) {
+                    console.log("no image for this user: " + lastCommentedUsername);   
+                }
+
+                $("<div class=\"ItemContent Category CustSkin-ItemContent\"><\div>").appendTo(leftCol);
+                $("<div class=\"ItemContent Category CustSkin-ItemContent\"><\div>").appendTo(rightCol);
+
+                var leftDiscussionDiv = leftCol.getElementsByClassName('ItemContent Category')[0];
+                var rightDiscussionDiv = rightCol.getElementsByClassName('ItemContent Category')[0];
+
+                $(title).appendTo(leftDiscussionDiv);
+                $(categoryDescription).appendTo(leftDiscussionDiv);
+
+                $("<div class=\"Meta CustSkin-LatestCommenter\"><\div>").appendTo(rightDiscussionDiv);
+                var rightMetaDiv = rightDiscussionDiv.getElementsByClassName('Meta')[0];
+
+                //This fails on the NAS section
+                try {
+                    /*Title of last thread*/
+                    $(lastDiscussionTitle.getElementsByTagName('a')[0]).appendTo(rightMetaDiv);
+                    /*Last replied user*/
+                    $(lastDiscussionTitle.getElementsByTagName('a')[1]).appendTo(rightMetaDiv);
+                } catch(e) {
+                    //Do nothing   
+                }
+                $(lastCommentDate).appendTo(rightMetaDiv);
+                $(meta).appendTo(leftDiscussionDiv);
+                $(itemContent).remove();
+
+            }
         }
     }
 }
